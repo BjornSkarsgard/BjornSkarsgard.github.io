084aa681694bcc04abe3c9b457c1faf0f3cf5ebe
Bjorn Skarsgard <49474414+BjornSkarsgard@users.noreply.github.com>
2019-09-14 14:21:57 +0500

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 2000,
+        value: 200,
         density: {
           enable: true,
           value_area: 800
