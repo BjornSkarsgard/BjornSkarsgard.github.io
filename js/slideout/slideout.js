4d3d62f742df495b55f8eb279b2af1252fcbd94b
Bjorn Skarsgard <49474414+BjornSkarsgard@users.noreply.github.com>
2019-09-30 19:48:21 +0500

    Add files via upload

--- js/slideout/slideout.js
+++ js/slideout/slideout.js
@@ -3,25 +3,14 @@
 var aside = document.querySelector(".sidebar__mobile");
 var slideout = document.querySelector(".slideout");
 var materials = document.querySelector(".wrapper");
+var burgerActive = document.querySelector(".slideout__burger")
 var body = document.body;
 
-
-document.documentElement.classList.remove("no-js");
-console.log("test");
-
 slideout.addEventListener("click", function(evt) {
     evt.preventDefault();
-    materials.classList.toggle("wrapper-transform");
+    materials.classList.toggle("wrapper-transform"),
+        aside.classList.toggle("sidebar-open-mobile"),
+        burgerActive.classList.toggle("slideout__burger--active");
+    // body.classList.toggle("overflow-hide"),
+    // aside.classList.toggle("overflow-visible");
 });
-
-slideout.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    aside.classList.toggle("sidebar-open-mobile");
-});
-
-
-
-// slideout.addEventListener("click", function(evt) {
-//     evt.preventDefault();
-//     materials.classList.toggle("materials-transform");
-// });
--- js/slider/slider.js
+++ js/slider/slider.js
@@ -1,27 +1,7 @@
-// $(document).ready(function () {
-//     $('.categories__dropdown-items > a').click(function () {
-//         $('.categories__dropdown-items ul').slideUp();
-//         if ($(this).next().is(":visible")) {
-//             $(this).next().slideUp();
-//         } else {
-//             $(this).next().slideToggle();
-//         }
-//         return false;
-//     });
-//     $('.categories__navigation > ul > li > a').click(function () {
-//         $('.categories__navigation > ul > li > a, .categories__dropdown-items a').removeClass('active');
-//         $(this).addClass('active');
-//     }),
-//         $('.categories__dropdown-items ul li a').click(function () {
-//             $('.categories__dropdown-items ul li a').removeClass('active');
-//             $(this).addClass('active');
-//         });
-// });
-
-
-
+"use strict";
 
 $(document).ready(function() {
+    // Отслеживаем клик и показываем другие категории в слайдере
     $('.sidebar__pc > .categories > .categories__navigation > ul > .categories__dropdown-items > a').click(function() {
         $('.categories__dropdown-items ul').slideUp();
         if ($(this).next().is(":visible")) {
@@ -41,8 +21,6 @@ $(document).ready(function() {
         }
         return false;
     });
-
-
 
 
 
